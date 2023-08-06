500a467dc726acbc3335186516cb0b4dc6c15a45
Yash Godara <yash17godara@gmail.com>
2023-08-06 15:37:33 +0530

    deploy app

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+async function Page() {
+    return <SignUp />;
+}
+
+export default Page
---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Threads App</h1>
+    </main>
+  )
+}
