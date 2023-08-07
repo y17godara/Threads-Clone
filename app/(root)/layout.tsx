7e52b4693a2458149e7eb90330cdd3fad38495ab
Yash Godara <yash17godara@gmail.com>
2023-08-07 16:00:18 +0530

    updates

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
             <TopBar />
             <main>
               <LeftSidebar />
-              <section className="main-container">
+              <section className="flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
                 <div className="w-full max-w-4xl">{children}</div>
               </section>
               <RightSidebar />
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,7 +1,25 @@
+import { sidebarLinks } from "@/constants";
 import React from "react";
+import Link from "next/link";
 
 function LeftSidebar() {
-  return <div>LeftSidebar</div>;
+  return (
+    <>
+      <section className="custom-scrollbar leftsidebar">
+        <div className="flex w-full flex-1 flex-col px-6">
+          {sidebarLinks.map((link) => (
+            <Link 
+              key={link.label}
+              href={link.route}
+              className=""
+            >
+              Link
+            </Link>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default LeftSidebar;
