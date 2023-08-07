c09224a2a3dafbf8753d4d0705adc451f68a6dfd
Yash Godara <yash17godara@gmail.com>
2023-08-07 14:21:40 +0530

    Updates

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,14 @@
-import "../globals.css";
+import "@styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import {
+  TopBar,
+  BottomBar,
+  LeftSidebar,
+  RightSidebar,
+} from "@components/index";
 
 export const metadata: Metadata = {
   title: "Threads",
@@ -19,7 +26,21 @@ export default function RootLayout({
     <>
       <ClerkProvider>
         <html lang="en">
-          <body className={`${inter.className}`}>{children}</body>
+          <body className={`${inter.className} `}>
+            {/* App */}
+            <TopBar />
+            <main>
+              <LeftSidebar />
+              <section className="main-container">
+                <div className="w-full max-w-4xl">
+                  {children}
+                </div>
+              </section>
+              <RightSidebar />
+            </main>
+            <BottomBar />
+            {/* App */}
+          </body>
         </html>
       </ClerkProvider>
     </>
