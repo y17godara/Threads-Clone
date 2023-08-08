1ab74139e4142c2f28f61e5291b7a426b6417cb0
Yash Godara <yash17godara@gmail.com>
2023-08-08 23:59:11 +0530

    Onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,8 +1,41 @@
+import { AccountProfile } from "@components/index";
+import { currentUser } from "@clerk/nextjs";
+
+interface Props {
+  user: number;
+  userInfo: {
+    _id: string;
+  };
+}
+
 async function Page() {
+  const user = await currentUser();
+
+  const userInfo = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username, 
+    name: userInfo?.name || user?.firstName + ' ' + user?.lastName || '',
+    bio: userInfo?.bio || '',
+    image: userInfo?.image || user?.imageUrl || '/assets/avtUrl.jpg',
+  }
+
   return (
     <>
-      <main>
+      <main className="mx-auto flex max-w-3x1 flex-col justify-start px-20 py-10">
         <h1 className="head-text">Onboarding</h1>
+        <p className="mt-3 text-base-regular text-light-2">
+          Complete your profile now to use Threads
+        </p>
+
+        <section className="mt-9 bg-dark-2 p-10">
+          <AccountProfile
+            user={userData}
+            btnTitle="Continue"
+          />
+        </section>
       </main>
     </>
   );
--- components/index.tsx
+++ components/index.tsx
@@ -4,5 +4,8 @@ import BottomBar from "./shared/BottomBar";
 import LeftSidebar from "./shared/LeftSidebar";
 import RightSidebar from "./shared/RightSidebar";
 
+// Import the Forms
+import AccountProfile from "./forms/AccountProfile";
+
 // Export the components
-export { TopBar, BottomBar, LeftSidebar, RightSidebar };
+export { TopBar, BottomBar, LeftSidebar, RightSidebar, AccountProfile };
