b4d830ce0cd922766b41ccc1ddc548744f5966f6
Yash Godara <yash17godara@gmail.com>
2023-08-15 18:20:27 +0530

    uploadingThing Setup

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,8 +1,8 @@
-import * as z from 'zod';
+import * as z from "zod";
 
 export const userValidation = z.object({
-    profile_photo: z.string().url().nonempty(),
-    name: z.string().min(4).max(24).nonempty(),
-    username: z.string().min(4).max(24).nonempty(),
-    bio: z.string().max(500).nonempty(),
+  profile_photo: z.string().url().nonempty(),
+  name: z.string().min(4).max(24).nonempty(),
+  username: z.string().min(4).max(24).nonempty(),
+  bio: z.string().max(500).nonempty(),
 });
